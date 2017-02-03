10ccb117ed5a38ac877849225e7e4e10930ced1e
Anoop Elias <anoopelias@gmail.com>
2017-02-03 17:51:00 +0530

    Challenge 3 complete

--- rust-matsano/src/challenge3.rs
+++ rust-matsano/src/challenge3.rs
@@ -2,26 +2,33 @@ use rustc_serialize::hex::FromHex;
 use std::str;
 use std::collections::HashMap;
 
+static ENGLISH_CHAR_FREQ: [char; 7] = ['e', 't', 'a', 'o', 'i', 'n', ' '];
+
 pub fn run() {
     let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
     let input_bytes = input.from_hex().unwrap();
 
-    for i in 88..89 {
+    for i in 0..150 {
         let xor_bytes = vec![i;input_bytes.len()];
         let new_bytes = xor(&input_bytes, xor_bytes);
 
-        let new_str = match str::from_utf8(&new_bytes){
-            Ok(string) => string,
-            Err(_) => "abcd"
+        match str::from_utf8(&new_bytes){
+            Ok(string) => score(string, &i),
+            Err(_) => {}
         };
-        println!("i:{} : {:?}", i, new_str);
 
-        english_score(new_str);
     }
 }
 
-fn char_freq(string: &str) -> Vec<(char, i32)> {
+fn score(string: &str, i: &u8) {
+    let score = english_score(String::from(string));
+    if score > 4 {
+        println!("Challenge 3 : {} :: {}  :: {}", i, score, string);
+    }
+}
+
+fn char_freq(string: &String) -> Vec<(char, i32)> {
     let mut char_map = HashMap::new(); 
 
     for c in string.chars() {
@@ -36,18 +43,26 @@ fn char_freq(string: &str) -> Vec<(char, i32)> {
     chars
 }
 
-fn remove_spaces(string: &str) -> &str {
-
+fn clean(string: &String) -> String {
+    string.to_lowercase()
 }
 
-fn english_score(string: &str) -> i32 {
-    let freq = char_freq(string);
-    println!("Freq : {:?}", freq);
+fn english_score(string: String) -> i32 {
+    let clean_string = clean(&string);
+    let freq = char_freq(&clean_string);
+    let length = ENGLISH_CHAR_FREQ.len();
 
-    if freq[0].0 == 'e' {
-        println!("String : {:?}", string);
-    }
-    0
+    let top_freq = freq[0..length].to_vec();
+    //println!("Freq : {:?}", top_freq);
+    let score = top_freq.iter().fold(0, |score, &(ch, _)| {
+        if ENGLISH_CHAR_FREQ.contains(&ch) {
+            score + 1
+        } else {
+            score
+        }
+    });
+
+    score
 }
 
 fn xor(ls1: &Vec<u8>, ls2: Vec<u8>) -> Vec<u8> {
